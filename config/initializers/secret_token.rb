61aad8adf96009e54f37548b397fa7522383c34b
tranducliem <duyliempro@gmail.com>
2013-11-11 13:44:26 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookOnline::Application.config.secret_key_base = '1c4ac5407010338f4e771f316f327cb3a12ef54c74941f9b6c71a22774344f6ae46f041430b5024b2a00ac42d4710e8adf4194263a86002e865906d800c22ca7'
